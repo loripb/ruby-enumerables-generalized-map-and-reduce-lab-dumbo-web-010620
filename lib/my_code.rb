a8c7482e8bf83679357270bf064f368e2ef03706
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 09:35:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(arr)
+  result = []
+  i      = 0
+  while i < arr.length do
+    result << yield(arr[i])
+    i += 1
+  end
+  result
+end
+
+def reduce(arr, starting_point=0)
+  result = starting_point
+  i      = 0
+
+  while i < arr.length
+    result = yield(arr[i], result)
+    i += 1
+  end
+
+  if result
+    return result
+  else
+    return false
+  end
+end
